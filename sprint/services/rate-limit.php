77bfd657b65c6c9819c7b21f3c043c7d8d8d8064
Storm <42808385+StormTersteeg@users.noreply.github.com>
2022-11-07 18:55:01 +0100

    Update rate-limit.php

--- sprint/services/rate-limit.php
+++ sprint/services/rate-limit.php
@@ -1,4 +1,13 @@
 <?php
+// Service to negate cpu usage during a request spam.
+// Set $rate_limit equal to the max-amount of request you want to allow per second.
+
+// Example:
+// $rate_limit = 4;
+
+// Any request that exceeds this limit will be "ignored" by serving a blank document.
+
+$rate_limit = 4;
 $marker = date("s");
 
 if (isset($_SESSION['rate-limit-stamp'])) {
